5534a68aa71845c919635e9fed943980070b7a39
notadeejay <amyemansell@gmail.com>
2020-07-11 15:25:51 -0400

    added nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,10 +7,12 @@ export default function Nav(props) {
 
   return (
   <div className = "nav">
+      <p>What would you like to make? A</p>
       <a href="/" name="callout" onClick={event => {
               event.preventDefault()
               props.handleClick(event.target.name);
               }}>Callout</a>
+              <p>or a</p>
       <a href="/" name="dropdown" onClick={event => {
               event.preventDefault()
               props.handleClick(event.target.name);
